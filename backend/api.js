b2a7229d8ef45c11c591b4e0c98dd282ce6a6866
Samrat Sahoo <samratsahoo2013@gmail.com>
2023-08-23 14:14:26 -0400

    Enable cross origin requests

--- backend/api.js
+++ backend/api.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { database } from './database.js';
+import cors from "cors";
 
 const app = express();
 const port = 5000;
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
